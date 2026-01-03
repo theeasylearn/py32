2a5e8aa9d18dcd9e5ceee664f294c4952dc30e93
theeasylearn <theeasylearn@gmail.com>
2026-01-03 11:09:47 +0530

    string and list

---
+++ 2_string.py
@@ -0,0 +1,16 @@
+name = "THE EASYLEARN ACADEMY"
+city = " Bhavnagar"
+print(name) #THE EASYLEARN ACADEMY
+print(name[0]) #T
+print(name[1]) #H
+print(name[2]) #E
+print(name[3]) #
+print(name[0:3]) # THE
+print(name[4:13]) # EASYLEARN
+print(name[14:]) # ACADEMY
+print(name+city)
+print(city*5)
+city = "Baroda"
+print(city)
+# city[0] = 'x' #error
+print('good bye')
---
+++ 3_list_1.py
@@ -0,0 +1,15 @@
+#example of list 
+box = ['Mouse',100,3.14,True,None] #mixed type
+marks = [50,75,85,95,65]
+print(box)
+print(marks)
+#get 1st item in box 
+print(box[0])
+
+#get 2 items from begining 
+print(box[0:2]) 
+
+#last 3 times 
+print(box[2:])
+print(marks*2) #print list twice
+print(box+marks) #join(concatenate) and display both list 
